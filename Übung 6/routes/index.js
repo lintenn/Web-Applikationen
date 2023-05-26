c8e9fb126759b1b93888682d2fd79a5e9e69afde
Garcia Marin <lg89614@win.edag.de>
2023-05-26 13:20:13 +0200

    Update ubung 6

--- Übung 6/routes/index.js
+++ Übung 6/routes/index.js
@@ -16,14 +16,29 @@ router.get('/person', function(req, res, next) {
 });
 
 router.get('/newPerson', function(req, res, next) {
+  console.log("Last name: " + req.query.lastName);
+  console.log("E-Mail: " + req.query.email);
+  console.log("Birthday: " + req.query.birthday);
+
+
+  let now = new Date(Date.now()).getTime();
+  let bday = new Date(req.query.birthday).getTime();
+  console.log(now);
+  console.log(bday);
+  let difference = now - bday;
+  console.log(difference);
+  difference = difference / (1000 * 60 * 60 * 24);
+  difference = Math.floor(difference);
+
+  console.log("Days alive: " + difference);
+
+  res.send("Daten per GET erhalten! Name: " + req.query.lastName);
   
-  res.send("Daten per GET erhalten! Name: " + req.query.name);
-  console.log(req.query.name);
 });
 
 router.post('/newPerson', function(req, res, next) {
-  console.log(req.body.name);
-  res.send("Daten per POST erhalten!");
+  console.log(req.query.lastName);
+  res.send("Daten per POST erhalten! Name: " + req.query.lastName);
 });
 
 module.exports = router;
